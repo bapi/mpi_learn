83d6ac8a28e3fa4314d2ed621e30c3bd63a47bb7
U-IST\bchatter <bchatter@wndaal008.ist.local>
2019-07-23 08:00:35 +0200

    changes

--- mpi_learn/logger.py
+++ mpi_learn/logger.py
@@ -61,7 +61,7 @@ class MPIFileHandler(logging.FileHandler):
         try:
             msg = self.format(record)
             stream = self.stream
-            stream.Write_shared((msg+self.terminator).encode('utf-8').strip())
+            stream.Write_shared((msg+self.terminator).encode('ISO-8859-1').strip())
             #self.flush()
         except Exception:
             self.handleError(record)
